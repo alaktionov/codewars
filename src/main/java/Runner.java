a3e3cc6db6eec2c845d85c30d1ed012b76db1e72
artem_laktionov <artem_laktionov@epam.com>
2022-02-06 14:40:03 +0100

    Implement TwoSum.java

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,8 +1,10 @@
-import leetcode.ValidatePassword;
+import leetcode.TwoSum;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Runner {
 
     public static void main(String[] args) {
-        System.out.println(ValidatePassword.isPasswordComplex("Dababa3"));
+        assertEquals(TwoSum.twoSum(new int[]{2, 5, 5, 15}, 10), new int[]{0, 1});
     }
 }
---
+++ src/main/java/leetcode/TwoSum.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ *
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * You can return the answer in any order.
+ */
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        int[] indexes = new int[2];
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    indexes[0] = i;
+                    indexes[1] = j;
+                    return indexes;
+                }
+            }
+        }
+        return indexes;
+    }
+}
